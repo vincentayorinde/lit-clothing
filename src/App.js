587c0a7cfd0ca84398cec0f3dbd69169fdac24ac
Vincent Ayorinde <vincentayorinde@yahoo.com>
2022-02-24 01:08:47 +0100

    broken home into smaller commponent

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Home } from './home.component'
+import { Home } from './pages/home/home.component'
 function App() {
 	return (
 		<div>
---
+++ src/pages/home/home.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './home.styles.scss'
+import Directory from '../../components/directory/directory.component'
+export const Home = () => {
+	return (
+		<div className='home'>
+			<Directory />
+		</div>
+	)
+}
